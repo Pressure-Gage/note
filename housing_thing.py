602684a03af8fcb7e5a84cc3317e140219a58f62
Pressure-Gage <159047157+Pressure-Gage@users.noreply.github.com>
2024-02-13 15:46:58 +0000

    gg

---
+++ housing_thing.py
@@ -0,0 +1,12 @@
+ans = input("do you want to buy a house? ").lower()
+
+
+if ans == 'yes':
+    bedrooms = int(input("how many bedrooms do you want? "))
+    if bedrooms <3:
+        bathrooms = int(input("how many bathrooms do you want? "))
+        if bathrooms <3:
+            square_feet = int(input("how much square footage are you looking for? "))
+    print("you have selected", bedrooms, "amount of bedrooms,", bathrooms, "amount of bathrooms, and", square_feet, "square footage")
+else:
+    print("Ok, have a nice day!")
