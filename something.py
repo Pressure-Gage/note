ba7edc1c2f0f043cee9f63d928ddf41101dfe3f2
Pressure-Gage <159047157+Pressure-Gage@users.noreply.github.com>
2024-02-29 16:24:06 +0000

    66666

--- something.py
+++ something.py
@@ -12,3 +12,14 @@ ans2 = add_it(5,6)
 ans3 = add_it(7,8)
 
 #define a function that determines if a number is even or odd
+
+def even_odd(x):
+    if x  %2 == 0:
+        return 'Even'
+    if x%2 !=0:
+        return 'Odd'
+
+num_1 = even_odd(8)
+print(num_1)
+num_2 = even_odd(9)
+print(num_2)
