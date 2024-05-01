276ab1fe80983f4cd864cba5f1f37bbd8ba72801
Pressure-Gage <159047157+Pressure-Gage@users.noreply.github.com>
2024-05-01 15:29:02 +0000

    bvb

--- encryption.py
+++ encryption.py
@@ -2,6 +2,9 @@ choice = 0
 dict1 = 0
 encoded_message = 0
 all_encoded_messages = ""
+password = 'password'
+
+username = input('what is your name?: ')
 print('''
   ####################
    1) Encode Message
@@ -10,14 +13,21 @@ print('''
        4) Exit
   ####################
 ''')
-choice = int(input('what would you like to do?: '))
+choice = int(input(f'hello {username}, what would you like to do?: '))
 if choice == 1:
-    message = input('what do you want to be encoded?: ')
-    encoded_message = ""
-    for i in range (len(message)):
-        encoded_message = encoded_message + chr(ord(message[i]) + 3)
-    print (encoded_message)
-    all_encoded_messages.append(encoded_message)
+  message = input('what do you want to be encoded?: ')
+  encoded_message = ""
+  for i in range (len(message)):
+    encoded_message = encoded_message + chr(ord(message[i]) + 3)
+  print (encoded_message)
+  all_encoded_messages.append(encoded_message)
 
 if choice == 2:
-    
+  input_password = input('what is the password').lower()
+  while input_password != 'password':
+    input_password = ('invalid password, try again: ')
+  decode = int(input(f'ok {username}, what would you like to decode?: '))
+  decoded_message = ""
+  for i in range (len(decode)):
+    decoded_message = decoded_message + chr(ord(decode[i]) + 3)
+  print (encoded_message)
--- testing.py
+++ testing.py
@@ -1,3 +1,38 @@
-import math
-speed_run = dict = {'John': [4.3, 6.7, 2.2, 7.8, 4.5, 3.2, 3.6],'Robin': [6.5, 3.4, 3.8, 4.5, 3.3, 9.2, 5.5],'Max': [2.5, 4.4, 4.2, 3.2, 3.2, 5.3, 4.2]}
-statistics.median(['Max'])
+choice = 0
+all_encoded_messages = []
+encoded_message = 0
+password = 'password'
+decode = ""
+input_passowrd = 0
+
+username = input('what is your name?: ')
+print('''
+  ####################
+   1) Encode Message
+   2) Decode Message
+  3) See All Messages
+       4) Exit
+  ####################
+''')
+choice = int(input(f'hello {username}, what would you like to do?: '))
+while choice == 1:
+  message = input('what do you want to be encoded?: ')
+  encoded_message = ""
+  for i in range (len(message)):
+    encoded_message = encoded_message + chr(ord(message[i]) + 3)
+  print (encoded_message)
+  all_encoded_messages.append(encoded_message)
+  print(all_encoded_messages)
+  choice = int(input("what would you like to do next?: "))
+
+while choice == 2:
+    input_password = input('what is the password: ').lower()
+    while input_password != 'password':
+        input_password = ('invalid password, try again: ')
+    else:
+        decode = input(f'ok {username}, what would you like to decode?: ')
+        decoded_message = ""
+        for i in range (len(message)):
+            decoded_message = decoded_message + chr(ord(decode[i]) - 3)
+    print (decoded_message)
+    choice = int(input("what would you like to do next?: "))
